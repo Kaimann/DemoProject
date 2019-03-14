dbe8b2ef5fa8276144a6e759f089f09cc4bb9a5d
Vladimir <vladimir.panov.1982@gmail.com>
2019-03-14 03:26:13 +0200

    demo2

--- src/lesson1/ex1.java
+++ src/lesson1/ex1.java
@@ -2,6 +2,7 @@ package lesson1;
 
 public class ex1 {
     public static void main(String[] args) {
-        System.out.println("Very Good!!!");
+        System.out.println("Very Good!!!"); // это демо
+        System.out.println("Ok!!!");
     }
 }
